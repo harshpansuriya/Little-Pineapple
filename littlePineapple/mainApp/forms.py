8ebdeabd933b9cf27959416e3274cc4460a5fb91
Harsh Pansuriya <harshpansuriya123@gmail.com>
2023-01-15 09:56:31 -0500

    15012023 - Changes(2)
    Remove Whole App

--- littlePineapple/mainApp/forms.py
+++
@@ -1,8 +0,0 @@
-from django.form import ModelForm
-from .models import Skillers
-
-
-class SkillersForm(ModelForm):
-    class Meta:
-        Model = Skillers
-        field = "__all__"
--- littlePineapple/mainApp/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Skillers
-
-class TodoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Skillers
-        fields = ('id', 'first_name', 'last_name', 'email', 'special_skill', 'skill_decs', 'tags')
-
