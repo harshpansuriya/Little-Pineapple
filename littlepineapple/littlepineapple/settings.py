98a551d398396fa51f2f00b2e9380f89274993e9
Neveel Hingu <99134102+23NH2001@users.noreply.github.com>
2023-01-16 14:24:28 -0500

    Update settings.py
    Remove mainApp from the Installed app in Settings.py

--- littlepineapple/littlepineapple/settings.py
+++ littlepineapple/littlepineapple/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mainapp.apps.MainappConfig',
     # New Apps
     'SkillersApp.apps.SkillersappConfig',
 ]
